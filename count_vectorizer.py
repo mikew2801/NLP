454ea5b9733729862aa954ef6d604e011dfddd4d
Michael WIlliams <mikew2801@gmail.com>
2021-09-05 09:00:26 +0000

    Update count_vectorizer.py

--- count_vectorizer.py
+++ count_vectorizer.py
@@ -49,21 +49,3 @@ class CountVectorizer:
         return self.vectorized_list
 
 
-horror = ["The death of hell is forever vampire", "Vampires live in the darkness of the night", "The night's horror is the hell of vampires, and forever do they dwell in hell", "The darkness of the world goes on forever", "Dark was the night as all the horror vampire nights before it", "Hell hath fury and darkness", "The darkness of the world is greedy and hellish", "hellish hellish hellish hellish"]
-
-romance = ["Love is sunshine and candy.", "We live in love and laughter and sunshine", "Candy and laughter are the most beautiful sensets", "Everything is beautiful in its own way and so is love and laughter", "I love candy and romance", "Sunsets are beautiful and laughter", "Beauty is the love of laughter."]
-
-X = horror + romance
-y = [0 for s in horror] + [1 for s in romance]
-
-cv = CountVectorizer()
-X = cv.fit_transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-from sklearn.linear_model import LinearRegression
-
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-print(lr.score(X_test, y_test))
-
